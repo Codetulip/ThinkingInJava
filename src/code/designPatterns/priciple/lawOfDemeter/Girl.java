7ac574ad1c122ff87abd381a98b7da1e1883fc9d
codetulip <447319303@qq.com>
2019-06-05 16:29:57 +0800

    Law of Demeter

---
+++ src/code/designPatterns/priciple/lawOfDemeter/Girl.java
@@ -0,0 +1,11 @@
+package code.designPatterns.priciple.lawOfDemeter;
+
+/**
+ * @Classname Girl
+ * @Description 女生类
+ * @Author tujing
+ * @Date 2019/6/5 3:55 PM
+ * @Version 1.0
+ */
+public class Girl {
+}
