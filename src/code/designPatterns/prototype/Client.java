b71d1fa0175a824ac5b4586939051f4aff4c07e8
codetulip <447319303@qq.com>
2019-06-13 21:13:12 +0800

    prototype pattern

---
+++ src/code/designPatterns/prototype/Client.java
@@ -0,0 +1,52 @@
+package code.designPatterns.prototype;
+
+import java.util.Random;
+
+/**
+ * @Classname Client
+ * @Description 场景类
+ * @Author tujing
+ * @Date 2019/6/13 4:11 PM
+ * @Version 1.0
+ */
+
+// 用原型实例指定创建对象的种类，并且通过拷贝这些原型创建新的对象
+public class Client {
+
+    // 发送邮件数量，这个值是从数据库中获得的
+    private static int MAX_COUNT = 6;
+
+    public static void main(String[] args) {
+        // 模拟发送邮件
+        int i = 0;
+        // 把模板定义出来，这个是从数据库中获得的
+//        Mail mail = new Mail(new AdvTemplate());
+        MailClone mail = new MailClone(new AdvTemplate());
+        mail.setTail("XX银行版权所有");
+        while (i < MAX_COUNT) {
+            // 以下是每封邮件不同的地方
+            MailClone mailClone = mail.clone();
+            mailClone.setAppellation(getRandString(5) + " 先生（女士)");
+            mailClone.setReceiver(getRandString(5) + "@" + getRandString(8)
+            + ".com");
+            sendMail(mailClone);
+            i++;
+        }
+    }
+
+    // 发送邮件
+    public static void sendMail(MailClone mail) {
+        System.out.println("标题：" + mail.getSubject() + "\t收件人：" + mail.getReceiver() + "\t...发送成功");
+    }
+
+    // 获得指定长度的随机字符串
+    public static String getRandString(int maxLength) {
+        String source = "aldjaoihdaoisdjnklasndasljndlasdkasn";
+        StringBuffer sb = new StringBuffer();
+        Random random = new Random();
+        for (int i = 0; i < maxLength; i++) {
+            sb.append(source.charAt(random.nextInt(source.length())));
+        }
+        return sb.toString();
+    }
+}
---
+++ src/code/designPatterns/prototype/Thing.java
@@ -0,0 +1,49 @@
+package code.designPatterns.prototype;
+
+import java.util.ArrayList;
+
+/**
+ * @Classname Thing1
+ * @Description 浅拷贝
+ * @Author tujing
+ * @Date 2019/6/13 5:09 PM
+ * @Version 1.0
+ */
+public class Thing implements Cloneable{
+
+    // 定义一个私有变量
+    private ArrayList<String> arrayList = new ArrayList<String>();
+
+    @Override
+    public Thing clone() {
+        Thing thing = null;
+        try {
+            thing = (Thing) super.clone();
+//            thing.arrayList = (ArrayList<String>) this.arrayList.clone(); // 深拷贝
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return thing;
+    }
+
+    // 添加arrayList值
+    public void setValue(String value) {
+        this.arrayList.add(value);
+    }
+
+    // 取出整个arrayList
+    public ArrayList<String> getValue() {
+        return this.arrayList;
+    }
+
+    public static void main(String[] args) {
+        // 产生一个对象
+        Thing thing = new Thing();
+        // 添加一个值
+        thing.setValue("张三");
+        // 拷贝一个对象
+        Thing thingClone = thing.clone();
+        thingClone.setValue("李四"); // 共用了一个List
+        System.out.println(thing.getValue()); // 输出结果为[张三,李四]
+    }
+}
