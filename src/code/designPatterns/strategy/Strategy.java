bf4caacc10f2da942692e9a3c7f42dd76995e178
codetulip <447319303@qq.com>
2019-06-30 12:45:23 +0800

    strategy pattern

---
+++ src/code/designPatterns/strategy/Strategy.java
@@ -0,0 +1,14 @@
+package code.designPatterns.strategy;
+
+/**
+ * @Interfacename Strategy
+ * @Description 抽象的策略角色
+ * @Author tujing
+ * @Date 2019/6/30 12:28 PM
+ * @Version 1.0
+ */
+// 定义一组算法，将每个算法都封装起来，并且使他们之间可以互换。
+public interface Strategy {
+    // 策略模式的运算法则
+    void doSomething();
+}
